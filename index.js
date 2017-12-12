85d5848c5d7866f562b91dd222aaa3b19bfeb8cf
gadrion <wjdrlehs57@naver.com>
2017-12-12 22:55:46 +0900

    동적으로 목차를 채워 넣도록 변경중.

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        message: `Hello Vue.js! I'm GD`
-    }
-})
+// var fs = require('fs');
+// fs.readFile('./index.html', 'utf8', function(err, data) {
+//   // the data is passed to the callback in the second argument
+//   console.log(data);
+// });
 
-const app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: `이페이지는 ${new Date()} 에 로드 되었다?`
-    }
-})
-
-const app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-})
-
-const app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'JavaScript 배우기' },
-            { text: 'vue 배우기' },
-            { text: '뭐라도 만들어 보자' }
-        ]
-    }
-})
-
-const app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: '안녕하세요!!! 테스트 중입니다.'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
         }
     }
-})
+    rawFile.send(null);
+}
+
+const initIndex = (text) => {
+    const index_json = JSON.parse(text);
+    for(const capter of index_json.capters) {
+        console.log(capter);
+    }
+};
+
+(() => {
+    readTextFile("index.json", initIndex);
+})();
